0c2dd626769874fdf7eb6de08b518628ab38b8f8
CongQuy252 <quyok25203@gmail.com>
2025-03-05 22:39:08 +0700

    update home page, profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './pages/HomePage/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound"; // Trang 404 (Tùy chọn)
+import Layout from "./components/Layout"; // Layout chứa header, footer
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
-      
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Route cho trang chủ */}
-        
-        {/* <Route path="*" element={<NotFoundPage />} /> Route cho các trang không tìm thấy (ví dụ) */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <Profile />
+            </Layout>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} /> {/* Route cho trang 404 */}
       </Routes>
     </Router>
-  )
+  );
 }
-export default App
+
+export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import styles from './Footer.module.css';
-function Footer(){
-    return(
-        <div  className={styles.sidebar}>   
-            <h2>Welcome to the Home Page!</h2>
-            <p>This is the content of the home page.</p>
-      </div >
-    );
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <div className={styles.sidebar}>
+      <h2>Welcome to my Page!</h2>
+      <p>Phát triển bởi OnlyA Team</p>
+    </div>
+  );
 }
 export default Footer;
