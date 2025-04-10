c74a04e025f517d324cab4823d07c33edd1e2eb0
CongQuy252 <quyok25203@gmail.com>
2025-04-10 14:41:36 +0700

    update env deploy

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = `https://${window.location.hostname}:${
-  import.meta.env.VITE_PORT
-}/api/chats/`;
+const API_URL = `https://${import.meta.env.VITE_HOST}/api/chats/`;
 
 const chatService = {
   getChatRoom: async (chatRoomId) => {
