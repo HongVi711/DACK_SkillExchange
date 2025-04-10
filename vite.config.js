31b1f959ff76945349c3d16e7c6c4bb119aa16e9
CongQuy252 <quyok25203@gmail.com>
2025-04-10 15:06:57 +0700

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@ import { defineConfig } from "vite";
 import fs from "fs";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync("key.pem"), // Đường dẫn đến key.pem
-      cert: fs.readFileSync("cert.pem") // Đường dẫn đến cert.pem
-    },
-    host: "0.0.0.0", // Cho phép truy cập từ thiết bị khác
-    port: 5173
-  },
-  plugins: [react()]
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve"; // "serve" là khi chạy `npm run dev`
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+          https: {
+            key: fs.readFileSync("key.pem"),
+            cert: fs.readFileSync("cert.pem")
+          },
+          host: "0.0.0.0",
+          port: 5173
+        }
+      : undefined // bỏ server config khi build/deploy
+  };
 });
